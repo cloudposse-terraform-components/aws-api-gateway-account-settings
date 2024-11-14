faae6638cf296dc8ce95f5b9e00b57d68365ffb9
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:18:04 +0100

    Initial commit

--- src/main.tf
+++ src/main.tf
@@ -1,8 +1,6 @@
-locals {
-  enabled = module.this.enabled
+module "api_gateway_account_settings" {
+  source  = "cloudposse/api-gateway/aws//modules/account-settings"
+  version = "0.3.1"
+
+  context = module.this.context
 }
-
-
-
-
-
--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,4 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "role_arn" {
+  description = "Role ARN of the API Gateway logging role"
+  value       = module.api_gateway_account_settings.role_arn
 }
